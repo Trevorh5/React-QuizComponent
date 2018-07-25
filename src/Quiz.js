44ca449c3b9ee797ee439b1c0140f5adafbb37b3
trevor <trevor.hallett.5@gmail.com>
2018-07-25 14:05:10 -0600

    Creating Quiz Class

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,3 +1,18 @@
 import React, { Component } from 'react';
 
 let quizData = require('./quiz_data.json');
+
+class Quiz extends Component{
+
+
+    render(){
+        return(<div>
+            <div className={QuizQuestion}></div>
+        </div>)
+    }
+
+    constructor(props){
+        super(props);
+        this.state = {quiz_position: 1};
+    }
+}
